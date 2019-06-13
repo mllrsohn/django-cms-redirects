6a7948d210644c8c090657974321b650b1ec76e3
SachaMPS <sendsend@gmx.de>
2019-06-13 18:04:10 +0200

    master: fix page node site django cms 3.6.0

--- cms_redirects/models.py
+++ cms_redirects/models.py
@@ -19,24 +19,24 @@ class CMSRedirect(models.Model):
         help_text=_("This can be either an absolute path (as above) or a full URL starting with 'http://'."))
     response_code = models.CharField(_('response code'), max_length=3, choices=RESPONSE_CODES, default=RESPONSE_CODES[0][0],
         help_text=_("This is the http response code returned if a destination is specified. If no destination is specified the response code will be 410."))
-    
+
     def page_site(self):
         if self.page:
-            return u'%s' % self.page.site
+            return '%s' % self.page.node.site
         return u''
     page_site.short_description = "Page Site"
-    
+
     def actual_response_code(self):
         if self.page or self.new_path:
             return self.response_code
         return u'410'
     actual_response_code.short_description = "Response Code"
-    
+
     class Meta:
         verbose_name = _('CMS Redirect')
         verbose_name_plural = _('CMS Redirects')
         unique_together=(('site', 'old_path'),)
         ordering = ('old_path',)
-    
+
     def __unicode__(self):
         return "%s ---> %s" % (self.old_path, self.new_path)
